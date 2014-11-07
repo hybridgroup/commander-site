6c5facc69c75bad58c7548e095acac9b7bfa8c2c
Luis Felipe Perez <slashcat@gmail.com>
2014-11-07 12:12:59 -0600

    configuration

---
+++ config.rb
@@ -0,0 +1,10 @@
+set :css_dir,    'stylesheets'
+set :js_dir,     'javascripts'
+set :images_dir, 'images'
+
+activate :directory_indexes
+activate :neat
+activate :livereload
+
+configure :build do
+end
